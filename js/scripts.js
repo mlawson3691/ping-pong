fb1a1b3c60f3a6d422937f3aaccec90ffcc2d4dc
Mark Lawson <mlawson3691@gmail.com>
2016-08-19 08:41:45 -0700

    create basic user interface logic to receive input and display results to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+// ===== BACK END LOGIC ===== //
 
+var count = function(number) {
+
+}
+
+// ===== FRONT END LOGIC ===== //
 
 $(function() {
-  
+  $("#submit").click(function() {
+    var numberInput = parseInt($("#numberInput").val());
+    var result = count(numberInput);
+    $("#result").text(result);
+  });
 });
