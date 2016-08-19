607454ea1e4afd9524426ecd1a37352e7e2aa125
Mark Lawson <mlawson3691@gmail.com>
2016-08-19 08:48:08 -0700

    return ping if the number is divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@
 var count = function(number) {
   var numbers = [];
   for (var i = 1; i <= number; i++) {
-    numbers.push(i);
+    if (i % 3 === 0) {
+      numbers.push("ping");
+    } else {
+      numbers.push(i);
+    }
   }
   return numbers;
 }
