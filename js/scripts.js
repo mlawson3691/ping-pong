5dab10d33003226fbd04b3284bbcabdcec197d50
Mark Lawson <mlawson3691@gmail.com>
2016-08-19 08:45:40 -0700

    return a list of numbers up to and including the inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 // ===== BACK END LOGIC ===== //
 
 var count = function(number) {
-
+  var numbers = [];
+  for (var i = 1; i <= number; i++) {
+    numbers.push(i);
+  }
+  return numbers;
 }
 
-// ===== FRONT END LOGIC ===== //
+// ===== USER INTERFACE LOGIC ===== //
 
 $(function() {
-  $("#submit").click(function() {
+  $("#submit").click(function(event) {
+    event.preventDefault();
     var numberInput = parseInt($("#numberInput").val());
     var result = count(numberInput);
     $("#result").text(result);
